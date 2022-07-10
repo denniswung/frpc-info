92551fe3f0f6455464a91330c0fa0562b83a0476
WendyNg <57582289+xuanfau@users.noreply.github.com>
2022-07-10 17:56:33 +0800

    Update ssi.sh

--- ssi.sh
+++ ssi.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 source /etc/profile
 
+echo "install ss"
 apt install shadowsocks-libev -y
 
+echo "install brook"
 bash <(curl https://bash.ooo/nami.sh)
 
 nami install brook
 
+echo "INIT"
 cd /etc/shadowsocks-libev/
 
 curl -O https://github.com/xuanfau/frpc-info/blob/main/config.json
 
+echo "Start"
 sudo /etc/init.d/shadowsocks-libev start
 
 nohup brook relay -f :5001 -t 127.0.0.1:35697 > /dev/null 2>&1 &
